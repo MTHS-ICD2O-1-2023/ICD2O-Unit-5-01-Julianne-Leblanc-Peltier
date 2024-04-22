5b3f4177fb21bed85ceca78e3c463766e853434f
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2024-04-22 18:48:11 +0000

    js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Julianne Leblanc-Peltier
+// Created on: April 2024
 // This file contains the JS functions for index.html
+
+// variable
+const randomNumber = Math.floor(Math.random() * 6) + 1 // returns a random integer from 1 to 6 into variable "randomNumber"
+
+function myButtonClicked() {
+  if (userNumberGuess == randomNumber) {
+    // input
+    const userNumberGuess = document.getElementById("user-number-guess").innerHTML
+
+    
+  }
+}
